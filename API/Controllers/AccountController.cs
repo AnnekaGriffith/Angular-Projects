aa7e5a99be65699223223f0e4002dbfe4baa14ae
Anneka Bath <anneka.griffith@g.austincc.edu>
2021-01-15 02:04:43 -0600

    End section 4 ()

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,22 +4,25 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController :BaseApiController
+    public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
+        private readonly ITokenService _tokenSrevive;
 
-        public AccountController(DataContext context) 
-        { 
-            _context = context; 
+        public AccountController(DataContext context, ITokenService tokenSrevive)
+        {
+            _tokenSrevive = tokenSrevive;
+            _context = context;
         }
         //used to add to the end point
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
             //when using a dyspose method it calls upon a dispose interface 
@@ -34,25 +37,34 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenSrevive.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)    
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user =await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
-            
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
             if (user == null) return Unauthorized("Invalid username");
-            
+
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for (int i = 0; i< computedHash.Length; i++)
+            for (int i = 0; i < computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
-            return user;
-        }    
+            
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenSrevive.CreateToken(user)
+            };
+        }
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
